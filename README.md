f738a7b18bc64cb7a00704d7ddd2b9821546f6be
Juan Ignacio Perez <Juan Ignacio Perez>
2017-10-30 16:15:29 -0500

    base structure

---
+++ README.md
@@ -0,0 +1,17 @@
+## CondorLabs test
+
+#### Getting Started
+
+Clone this repository and run
+
+ > Windows
+
+```
+python -m http.server
+```
+
+ > MAC
+
+```
+python -m SimpleHTTPServer 8000
+```
---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+ * CondorLabs-landingPage
+ * 
+ * 
+ * @author Juan Perez
+ * @version 1.0.0
+ * Copyright 2017. MIT licensed.
+ */
+(function($){
+   $('.NavBar-menuColapsed').on('click', function (){
+    $('.NavBar-content').toggleClass('NavBar-content--show', 7000);
+   });
+})(jQuery);
+
